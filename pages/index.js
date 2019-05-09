877afa965c35dc8d16ede7ad45804ee2c94504dd
Marco <marco.mojica@artcom.de>
2019-05-09 22:49:26 +0200

    Add files

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react"
+
+const Index = () => {
+    const [Phaser, setPhaser] = useState(null)
+    const [Game, setGame] = useState(null)
+    const [scenes, setScenes] = useState(null)
+
+    // load Phaser
+    useEffect(() => {
+        setPhaser(require("phaser"))
+        setScenes(require("../scenes/"))
+    }, [])
+
+    if(Phaser && scenes && !Game){
+        setGame(new Phaser.Game({
+            type: Phaser.AUTO,
+            width: 800,
+            height: 600,
+            physics: {
+                default: "arcade",
+                arcade: {
+                    gravity: { y: 200 }
+                }
+            },
+            scene: new scenes.Main()
+        }))
+    }
+
+    return (
+        <div id="phaser"/>
+    )
+}
+
+export default Index
---
+++ scenes/index.js
@@ -0,0 +1,5 @@
+const { Main } = require("./main")
+
+export {
+    Main
+}
