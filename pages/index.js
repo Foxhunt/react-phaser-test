5a70796b73071b71be233e1e9aa4b130a49eb5b3
Marco <marco.mojica@artcom.de>
2019-05-14 15:51:05 +0200

    Remove game from state

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
-import React, { useState, useEffect } from "react"
+import React, { useEffect } from "react"
 
 const Index = () => {
-    const [game, setGame] = useState(null)
-
     // load Game
     useEffect(() => {
-        async function loadGame(){
-            const game = await import("../game")
-            setGame(game.default)
-        }
-        loadGame()
+        import("../game")
     }, [])
 
     return (
